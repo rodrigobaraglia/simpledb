5e28ddba44bef09583582d6b2a2af8a328ccdd61
Rodrigo D Baraglia Di Fulvio <ext_rdbaraglia@falabella.cl>
2021-01-14 22:12:46 -0300

    added cursor and refactored select and insert execution

--- table.c
+++ table.c
@@ -21,17 +21,6 @@ void deserialize_row(void *source, Row *destination)
     memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
-void *row_slot(Table *table, uint32_t row_num)
-{
-    uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void *page = get_page(table->pager, page_num);
-
-    uint32_t row_offset = row_num % ROWS_PER_PAGE;
-    uint32_t byte_offset = row_offset * ROW_SIZE;
-
-    return page + byte_offset;
-}
-
 Pager *pager_open(const char *filename)
 {
     int fd = open(filename, O_RDWR | O_CREAT, S_IWUSR | S_IRUSR);
@@ -172,6 +161,44 @@ void db_close(Table *table)
     }
     free(pager);
     free(table);
+}
+
+Cursor *table_start(Table *table)
+{
+    Cursor *cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = (table->num_rows == 0);
+    return cursor;
+}
+
+Cursor *table_end(Table *table)
+{
+    Cursor *cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = table->num_rows;
+    cursor->end_of_table = true;
+    return cursor;
+}
+
+void *cursor_value(Cursor *cursor)
+{
+    uint32_t row_num = cursor->row_num;
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void *page = get_page(cursor->table->pager, page_num);
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+
+    return page + byte_offset;
+}
+
+void cursor_advance(Cursor *cursor)
+{
+    cursor->row_num += 1;
+    if (cursor->row_num == cursor->table->num_rows)
+    {
+        cursor->end_of_table = true;
+    }
 }
 
 void print_row(Row *row)
--- table.h
+++ table.h
@@ -1,6 +1,7 @@
 #ifndef _TABLE_H_
 #define _TABLE_H_
 #include <stdint.h>
+#include <stdbool.h>
 #include "constants.h"
 
 #define COLUMN_USERNAME_SIZE 32
@@ -27,13 +28,23 @@ typedef struct Table
     Pager *pager;
 } Table;
 
+typedef struct Cursor
+{
+    Table *table;
+    uint32_t row_num;
+    bool end_of_table;
+} Cursor;
+
 void serialize_row(Row *source, void *destination);
 void deserialize_row(void *source, Row *destination);
-void *row_slot(Table *table, uint32_t row_num);
 void *get_page(Pager *pager, uint32_t page_num);
 Table *db_open(const char *filename);
 void db_close(Table *table);
 void free_table(Table *table);
+Cursor *table_start(Table *table);
+Cursor *table_end(Table *table);
+void *cursor_value(Cursor *cursor);
+void cursor_advance(Cursor *cursor);
 void print_row(Row *row);
 
 #endif
